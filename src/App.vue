590d2fb297c1bb992fb8eb05b6bd2a305bac47e7
Michael Kirsch <michael_kirsch@t-online.de>
2022-11-19 10:31:52 +0100

    jo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-app>
+		<v-app-bar app color="primary" dark>
+			<div class="d-flex align-center">
+				<v-avatar class="mr-1" size="50">
+					<v-icon size="50">mdi-safe</v-icon>
+				</v-avatar>
+				<h2>JS-Crack</h2>
+			</div>
+		</v-app-bar>
+
+		<v-main>
+			<HelloWorld />
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+	name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+	components: {
+		HelloWorld,
+	},
+
+	data: () => ({
+		//
+	}),
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,107 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<v-container>
+		<v-row class="text-center">
+			<v-col class="mb-4">
+				<h1 class="display-2 font-weight-bold mb-3 mt-4">
+					Welcome to JS-Crack
+				</h1>
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col cols="4"></v-col
+			><v-col cols="4">
+				<v-text-field
+					name="Hash"
+					label="Hash"
+					filled
+					append-icon="mdi-send"
+					id="id"
+				></v-text-field> </v-col
+			><v-col cols="4"></v-col>
+		</v-row>
+
+		<div class="d-flex justify-center">
+			<h1>Progress:</h1>
+		</div>
+		<div class="d-flex my-2 justify-center"></div>
+		<div class="d-flex justify-center">
+			<v-simple-table class="mr-6">
+				<template v-slot:default>
+					<thead>
+						<tr>
+							<th class="text-left">Name</th>
+							<th class="text-left">Calories</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="item in desserts" :key="item.name">
+							<td>{{ item.name }}</td>
+							<td>{{ item.calories }}</td>
+						</tr>
+					</tbody>
+				</template>
+			</v-simple-table>
+			<v-progress-circular
+				class="mt-5"
+				:rotate="360"
+				:size="100"
+				:width="15"
+				:value="10"
+				color="teal"
+			>
+				{{ 10 }}
+			</v-progress-circular>
+		</div>
+	</v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+	name: "HelloWorld",
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+	data: () => ({
+		desserts: [
+			{
+				name: "Frozen Yogurt",
+				calories: 159,
+			},
+			{
+				name: "Ice cream sandwich",
+				calories: 237,
+			},
+			{
+				name: "Eclair",
+				calories: 262,
+			},
+			{
+				name: "Cupcake",
+				calories: 305,
+			},
+			{
+				name: "Gingerbread",
+				calories: 356,
+			},
+			{
+				name: "Jelly bean",
+				calories: 375,
+			},
+			{
+				name: "Lollipop",
+				calories: 392,
+			},
+			{
+				name: "Honeycomb",
+				calories: 408,
+			},
+			{
+				name: "Donut",
+				calories: 452,
+			},
+			{
+				name: "KitKat",
+				calories: 518,
+			},
+		],
+	}),
+};
+</script>
